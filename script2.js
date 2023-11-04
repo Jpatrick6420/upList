6b589675f4feb227fa4db47d56f26292c5400abb
Justin Patrick <jpatrick6420@gmail.com>
2023-11-04 08:23:21 -0600

    1st commit

---
+++ script2.js
@@ -0,0 +1,30 @@
+function remove() {
+  list.shift();
+
+  liveList.innerHTML = `${list.join(", ")}`;
+}
+
+function adjustList() {
+  const name = newPerson.value.toLowerCase();
+
+  const newList = list.filter((item) => item !== name);
+
+  list = newList;
+  liveList.innerHTML = `${list.join(", ")}`;
+  newPerson.value = "";
+}
+
+// const arr = employees.filter((employee) =>
+//   employee.name.toLowerCase().includes("on")
+// );
+
+// let set = [];
+// console.log(arr);
+
+// pam.contacted = false;
+
+// console.log(arr);
+
+// set = [...arr];
+
+// //insertCard(set)
